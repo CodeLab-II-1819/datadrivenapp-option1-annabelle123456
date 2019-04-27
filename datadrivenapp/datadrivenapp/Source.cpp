709666fb0e80caf7266353f8c0b2a281d473bc7e
Annabelle <annabelle.greaves17@bathspa.ac.uk>
2019-04-27 13:46:06 +0100

    Added Search Function to main
    User can now search through Sample Tweets

--- datadrivenapp/datadrivenapp/Source.cpp
+++ datadrivenapp/datadrivenapp/Source.cpp
@@ -1,1 +1,34 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+string Tweets;
+string SearchingTweets;
+string mySearch;
+ifstream inFile;
+size_t myTweet;
+
+int main() {
+
+	inFile.open("sampleTweets.csv"); //Open sampleTweets.csv
+
+	cout << "type in a word to search: " << endl;
+	cin >> mySearch;
+	
+	if (!inFile) {
+		cout << "Unable to open file" << endl;
+	}
+
+	while (inFile.good()) {
+		getline(inFile, SearchingTweets);
+		myTweet = Tweets.find(mySearch);
+	}
+
+	system("pause");
+
+	return 0;
+
+}
